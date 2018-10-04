023a016948007fffd666725991f52e864f489085
Lovepreet Singh <lovepreet.singh61182@gmail.com>
2018-10-04 22:59:52 +0530

    Added nfa ot dfa and dfa from string

---
+++ dfa_from_string.py
@@ -0,0 +1,244 @@
+import itertools
+#input ending with string
+print("Enter ending with string :")
+st=input()
+#input aphabets set
+
+symbol=['a','b']
+for i in range(len(st)):
+    if st[i] not in symbol:
+        symbol.append(st[i])
+
+#generating porssible strings
+
+st1=''
+for s in symbol:
+    st1=st1+s
+print(st1)
+possiple_strings=[]
+for string in map(''.join, itertools.product(st1, repeat=len(st))):
+    possiple_strings.append(string.lower())
+possiple_strings.remove(st)
+# print(possiple_strings)
+
+nstates=len(st)+1
+
+#put states in states list
+
+states=[]
+for i in range(nstates):
+    states.append('q'+str(i))
+
+initial=states[0]
+final=states[len(st)]
+
+print("States used :"+str(states))
+
+# Default transitions
+
+transition={}
+def setTrans(states,st):
+    for i in range(len(st)):
+        var=(states[i],st[i])
+        transition[var]=states[i+1]
+
+setTrans(states,st)
+
+temptransition={}
+temptransition=transition.copy()
+
+#to check whether the transition on a state is valid or not
+def process(teststr,transitions,startstate,i,new,sym):
+    newtransitionset={}
+    if teststr[0]==sym:
+        newinitial = i
+
+    else:
+        newinitial=new
+   # print("initial "+newinitial)
+    start=startstate
+    n=0
+    if teststr=='':
+        return
+    var1=transitions.keys()
+    while start < nstates:
+        for k in var1:
+            if states[start] in k:
+                newtransitionset[k]=transitions[k]
+        start+=1
+   # print(newtransitionset)
+    trans=(newinitial,teststr[0])
+    loopcount=len(teststr)
+    var2=newtransitionset.keys()
+
+    for i in range(1,loopcount):
+        if teststr[i]=='':
+            return
+        for k in var2:
+            if k==trans:
+                change=newtransitionset[k]
+                trans=(newtransitionset[k],teststr[i])
+                #print(change)
+                #print(trans)
+                n+=1
+                break;
+
+    for k in var2:
+        if k==trans:
+            change=newtransitionset[k]
+            n+=1
+            break;
+
+    if n>=len(teststr):
+       return change
+    else:
+        return
+
+#Transtion function to set transitions
+
+def Transtions(states,symbol):
+    for i in range(nstates):
+        newi=nstates-i-1
+        count=newi+1
+        for j in range(len(symbol)):
+            key=transition.keys()
+            var=(states[newi],symbol[j])
+            if var not in key:
+                if newi==0:
+                    transition[var]=states[newi]
+                    temptransition[var]=states[newi]
+                else:
+                    if var not in key:
+                        for n in range(count):
+                            #print(states[newi-n])
+                           # if i == newi - 1 and states[newi] == states[newi - n]:
+                            #    continue;
+                            temptransition[var]=states[newi-n]
+                            #print(temptransition)
+                            value=process(st,temptransition,newi-n,states[newi],states[newi-n],symbol[j])
+                            val=[]
+                            for p in possiple_strings:
+                                val.append(process(p,temptransition,newi-n,states[newi],states[newi-n],symbol[j]))
+                                if states[newi]==states[nstates-1]:
+                                    val.append(process(p, temptransition, newi - n, states[newi-1], states[newi - n], symbol[j]))
+                            #print(final+","+str(value))
+                            if value==final and value  not in val:
+                                 transition[var]=states[newi-n]
+                                 break;
+                            else:
+                                del temptransition[var]
+
+Transtions(states,symbol)
+
+sorted(transition,key=transition.__getitem__)
+#print( "***********************Transitions:************************")
+#print(transition)
+
+key=transition.keys()
+
+#transitions a and b  aplphabet
+trans_A=[]
+trans_B=[]
+for each in key:
+    if 'a' in each:
+        trans_A.append(each)
+
+    else:
+        trans_B.append(each)
+
+print("********** Transition Table **********")
+print("States    ",end='')
+symbol.sort()
+for i in range(len(symbol)):
+    print(symbol[i]+"     ",end='')
+
+print("")
+
+for i in range(nstates):
+    if states[i] == final:
+        print("**" + states[i] + "     ", end='')
+        for k in trans_A:
+            if states[i] in k:
+                print(transition[k] + "     ", end='')
+
+        for k in trans_B:
+            if states[i] in k:
+                print(transition[k] + "     ", end='')
+        print("")
+
+
+    elif states[i]==initial:
+        print("->"+states[i] + "     ", end='')
+        for k in trans_A:
+            if states[i] in k:
+                print(transition[k] + "     ", end='')
+
+        for k in trans_B:
+            if states[i] in k:
+                print(transition[k] + "     ", end='')
+
+        print("")
+
+    else:
+        print("  "+states[i]+"     ", end='')
+        for k in trans_A:
+            if states[i] in k:
+                print(transition[k] + "     ", end='')
+
+        for k in trans_B:
+            if states[i] in k:
+                print(transition[k] + "     ", end='')
+
+        print("")
+
+#string processing
+
+def processstr(teststr):
+    if teststr=='':
+        return
+    var1=transition.keys()
+    trans=(initial,teststr[0])
+    loopcount=len(teststr)
+    for i in range(1,loopcount):
+        for k in var1:
+            if k==trans:
+                change=transition[k]
+                trans=(transition[k],teststr[i])
+                #print(change)
+                #print(trans)
+                break;
+
+    for k in var1:
+        if k==trans:
+            change=transition[k]
+            break;
+    return change
+
+while(True):
+     str=input("Enter string to process :")
+     result=processstr(str)
+
+     if result == final:
+         print("ACCEPTED ")
+
+     else:
+        print("Rejected ")
+     ans=input("Want to try more ?(Y/N)")
+     if(ans=='y'or ans=='Y'):
+         continue;
+
+     else:
+         break;
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nfa_to_dfa.py
@@ -0,0 +1,142 @@
+print(" Transition for NFA That you want to convert Using states(q0,q1,....) and alphabet (a.b) :")
+print("Enter Number Of States :")
+nstates=int(input())
+
+symbol=['a','b']
+states_nfa=[]
+for i in range(nstates):
+    states_nfa.append('q'+str(i))
+print("STATES")
+print(states_nfa)
+
+list1_nfa=[]
+for i in range(len(symbol)):
+    for j in range(nstates):
+        list1_nfa.append(states_nfa[j]+","+symbol[i])
+
+#print(list1_nfa)
+
+list2_nfa=[]
+for i in range(len(list1_nfa)):
+    print("Enter No. of transition for "+list1_nfa[i]+":")
+    n=int(input())
+    temp=[]
+    if n==0:
+        temp.append('#')
+    for j in range(n):
+        print("Enter state :")
+        temp.append(input())
+    list2_nfa.append(temp)
+
+#print("list2_nfa:")
+#print(list2_nfa)
+
+#input final and initial states
+initial='q0'
+print("Enter final state: ")
+final=input()
+
+#NFA to DFA
+list1=list1_nfa.copy()
+list2=list2_nfa.copy()
+
+states=[]
+symbol=['a','b']
+
+listnfa1=[]
+listnfa2=[]
+checklist=[['q0']]
+states=[['q0']]
+
+for i in range(len(list1)):
+    if 'q0' in list1[i]:
+        listnfa1.append(list1[i])
+        listnfa2.append(list2[i])
+
+#print("listnfa1: ")
+#print(listnfa1)
+while True:
+    for i in range(len(listnfa2)):
+        if listnfa2[i] not in checklist:
+            templist=[]
+            templist=listnfa2[i].copy()
+            print(templist)
+            nfa1=[]
+            nfa2=[]
+            nfa3=[]
+            nfa4=[]
+            for j in range(len(templist)):
+                for i in range(len(list1)):
+                    var=str(templist[j]+",a")
+                    var2=str(templist[j]+",b")
+                    if var in list1[i]:
+                        for k in range(len(list2[i])):
+                            nfa1.append(list2[i][k])
+                    elif var2 in list1[i]:
+                        for k in range(len(list2[i])):
+                            nfa3.append((list2[i][k]))
+            for d in nfa1:
+                if d not in nfa2 and d!='#':
+                    nfa2.append(d)
+            nfa1.sort()
+            nfa2.sort()
+            for d in nfa3:
+                if d not in nfa4 and d!='#':
+                    nfa4.append(d)
+            nfa3.sort()
+            nfa4.sort()
+            listnfa1.append(str(templist)+",a")
+            listnfa2.append(nfa2)
+            listnfa1.append(str(templist) + ",b")
+            listnfa2.append(nfa4)
+            checklist.append(templist)
+            if templist not in states:
+                states.append(templist)
+
+    #print("cheklist:")
+    #print(checklist)
+    tempchecklist=[]
+    for d in listnfa2:
+        if d not in tempchecklist and d != '#':
+            tempchecklist.append(d)
+    checklist.sort()
+    tempchecklist.sort()
+    #print("checklist :" + str(checklist))
+    #print("tempchecklist :" + str(tempchecklist))
+    if checklist==tempchecklist:
+        break
+
+#print(nfa1)
+#print(nfa2)
+#print(listnfa1)
+#print(listnfa2)
+#print(states)
+print("********** Transition Table **********")
+print("States"+" "*23,end='')
+symbol.sort()
+for i in range(len(symbol)):
+    print(symbol[i]+" "*(26-(len(str(symbol[i])))),end='')
+
+print("")
+count=0
+for i in range(len(states)):
+    if i==0:
+        print("->"+str(states[i])+" "*(23-(len(str(states[i])))),end='')
+        print("  "+str(listnfa2[count])+" "*(20-(len(str(listnfa2[count])))),end='')
+        print("  "+str(listnfa2[count+1]),end='')
+        print("")
+        count+=2
+    elif final in states[i]:
+        print("**" + str(states[i]) + " " * (23 - (len(str(states[i])))), end='')
+        print("  " + str(listnfa2[count]) + " " * (20 - (len(str(listnfa2[count])))), end='')
+        print("  " + str(listnfa2[count + 1]), end='')
+        print("")
+        count += 2
+    else:
+        print("  "+str(states[i]) + " " * (23 - (len(str(states[i])))), end='')
+        print("  "+ str(listnfa2[count]) + " " * (20 - (len(str(listnfa2[count])))), end='')
+        print("  "+ str(listnfa2[count + 1]), end='')
+        print("")
+        count += 2
+
+
